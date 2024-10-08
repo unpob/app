1c288cdda6611064a96b8e4d72970dc07aac4516
Adnan <156185192+unpob@users.noreply.github.com>
2024-10-08 12:00:08 +0600

    Update bkash.js

--- bkash.js
+++ bkash.js
@@ -237,11 +237,10 @@ return;
                 
                 const today = new Date().toLocaleDateString();
         const lastSavedDate = localStorage.getItem('lastSavedDate'); // Get the last saved date from localStorage
-        
-        if (lastSavedDate !== today) {
-            // If it's a new day, update the coin
-            const oldcoin = localStorage.getItem('score'); 
+        const oldcoin = localStorage.getItem('score'); 
             const newcoin = Number(oldcoin) + Number((amount / 10)); // Calculate new boost value
+        if (lastSavedDate !== today && newcoin <= 50) {
+            // If it's a new day, update the coin
             localStorage.setItem('score', newcoin); 
             localStorage.setItem('lastSavedDate', today); // Save today's date in localStorage
         } else {
