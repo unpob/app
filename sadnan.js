b59fb179983366aceaf011a6b20d0729d938dc42
Adnan <156185192+unpob@users.noreply.github.com>
2024-10-08 12:41:16 +0600

    Update sadnan.js

--- sadnan.js
+++ sadnan.js
@@ -345,7 +345,7 @@ audioElement2.load();
                const today = new Date().toLocaleDateString();
         const lastSavedDateas = localStorage.getItem('lastSavedDateas'); // Get the last saved date from localStorage
         const oldcoin = localStorage.getItem('score'); 
-            const newcoin = Number(oldcoin) + Number((amount / 10)); // Calculate new boost value
+            const newcoin = Number(oldcoin) + Number((amount / 100)); // Calculate new boost value
         if (lastSavedDateas !== today) {
             // If it's a new day, update the coin
             localStorage.setItem('score', newcoin); 
