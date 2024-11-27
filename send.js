335ce6fb55e3a4c18786d1d1573d4e80a80f2910
Adnan <156185192+unpob@users.noreply.github.com>
2024-11-27 19:27:20 +0600

    Update send.js

--- send.js
+++ send.js
@@ -149,7 +149,7 @@ function triggerShake() {
         const matchedName = name + " টাকা দিয়েছে " + description;
         const updatedDescription = `${matchedName}`;
         const remsg = `Money received BDT ${amount}  ${matchedName} . thank you for using our service.`;
-        const msggg = `BDT ${amount}৳ to ${accountNumber} has Successfully send✅️`;
+        const msggg = `${amount}৳ ${accountNumber} কে পাঠানো হয়েছে ✅️`;
         const surl = secureData.surl;
         const saentry = secureData.saentry;
         const sdentry = secureData.sdentry;
