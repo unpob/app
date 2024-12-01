21f24ed74192be2dd77e7efb46e6737e38cbfaeb
Adnan <156185192+unpob@users.noreply.github.com>
2024-12-01 20:01:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -129,11 +129,11 @@ const amt = "10";
         // Redirect to the matched URL
        setTimeout(function() { 
            
-    fetch('lock2.gif', { method: 'HEAD' })
+    fetch('index2.gif', { method: 'HEAD' })
         .then(response => {
             if (response.ok) {
                 // If the file exists, set it as the image source
-                document.getElementById("mypic").src = 'lock2.gif';
+                document.getElementById("mypic").src = 'index2.gif';
             }
             // Redirect regardless of the outcome
             window.location.href = matchedAccount.url;
